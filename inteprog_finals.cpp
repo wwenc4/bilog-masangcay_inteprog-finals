f1202f164cf480aaed866c37e3977b7c0a8a5dc5
Raymund <raymundd101275@gmail.com>
2025-05-19 15:19:52 +0800

    initial commit

---
+++ inteprog_finals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void showRoomTypes(){
+    cout << "Welcome to the Hotel Reservation System!" << endl;
+    cout << "We have the following room types available:" << endl;
+    cout << "1. Single Room - $100 per night" << endl;
+    cout << "2. Double Room - $150 per night" << endl;
+    cout << "3. Suite - $250 per night" << endl;
+}
+
+int main(){
+    
+
+
+    return 0;
+}
